36dd97d4cf5a4211901ec0d3afebc9ffb94496fa
David Atkinson <david.atkinson@netzero.com>
2019-09-04 22:11:36 -0500

    Initial commit

---
+++ JavaExampleCode/src/main/java/com/example/javaexamplecode/HelloWorld2.java
@@ -0,0 +1,11 @@
+package com.example.javaexamplecode;
+
+public class HelloWorld2 {
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[]) {
+        System.out.println("Hello, World");
+    }
+
+
+}
