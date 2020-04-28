1c59b7bb98290f3621d16e20fb0c2b8c0a4c8874
David Atkinson <david.atkinson@netzero.com>
2020-04-27 22:35:23 -0500

    Create Hello.java
    initial commit

---
+++ Replit/Hello.java
@@ -0,0 +1,13 @@
+// Hello
+// This is a very simple program. 
+// 
+// You can copy paste all of the test into repl.it and it will run.
+// Double forward slashes are comments and will not interfeer with the code.
+
+
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
